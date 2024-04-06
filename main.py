8c4f8e17a9bcd4de441ded420c5670b857916863
DragonBlade4419 <166095567+DragonBlade4419@users.noreply.github.com>
2024-04-06 05:29:21 +0000

    ALL NEW! TEXTUAL MAIN CLASS!

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Welcome Zyed Alsafi")
-passrint("their are Variable 1 transactions unchecked")
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+class BitcoinToolsApp(App):
+  BINDINGS = [("d", "toggle_dark", "Toggle dark mode")] 
+  def compose(self) -> ComposeResult:
+      yield Header()
+      yield Footer()
+  def action_toggle_dark(self) -> None:
+      self.dark = not self.dark
+if __name__ == "__main__":
+  app = BitcoinToolsApp()
+  app.run()
