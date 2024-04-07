a1a4e96f0ca601d86cd315ab82e2481f26fa1de8
DragonBlade4419 <166095567+DragonBlade4419@users.noreply.github.com>
2024-04-07 04:57:11 +0000

    Added label and changed header

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Label
 class BitcoinToolsApp(App):
-  BINDINGS = [("d", "toggle_dark", "Toggle dark mode")] 
-  def compose(self) -> ComposeResult:
-      yield Header()
-      yield Footer()
-  def action_toggle_dark(self) -> None:
-      self.dark = not self.dark
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    CSS_PATH = "bitcoin_tools.css""
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Label("Welcom, human!") 
+        yield Footer()
+    def action_toggle_dark(self) -> None:
+            self.dark = not self.dark
+    def on_mount(self) -> None:
+            self.title = "Open Wallet"
+    
 if __name__ == "__main__":
-  app = BitcoinToolsApp()
-  app.run()
+    app = BitcoinToolsApp()
+    app.run()
