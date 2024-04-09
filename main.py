282cace561beda3d537f97058fd488c02911fa20
DragonBlade4419 <166095567+DragonBlade4419@users.noreply.github.com>
2024-04-09 04:58:32 +0000

    Tupperware Time!🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱 🎉🐱
    /\_/\
    ( o.o )
     > ^ <
    🎉🐱

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Label
+from textual.widgets import Header, Footer, Label, Input, Button, Static
+from textual.containers import Center
 class BitcoinToolsApp(App):
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
-    CSS_PATH = "bitcoin_tools.tcss"
-    def compose(self) -> ComposeResult:
-        yield Header()
-        yield Label("Welcome, human!                                                               \n what name be?gtrfhfghbrtfgbdf", id="one")
-        #yield Label("Wasup my broskir67y8hugfygtuhgtyuhgftyuhygyftugytu?", id="two")
-        #yield Label("when the text above you has a stroke ",id="funny-text")
-        yield Footer()
-    def action_toggle_dark(self) -> None:
-            self.dark = not self.dark
-    def on_mount(self) -> None:
-            self.title = "Open Wallet"
+        BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+        CSS_PATH = "bitcoin_tools.tcss"
+        def compose(self) -> ComposeResult:
+            yield Header()
+            with Center():
+                yield Label("Welcome, human!\nwhat name be?", id="one")
+            with Center():
+                yield Input(placeholder="First Name")
+                yield Input(placeholder="Last Name")
+            yield Footer()
+        def action_toggle_dark(self) -> None:
+                        self.dark = not self.dark
+        def on_mount(self) -> None:
+                        self.title = "Open Wallet"
 if __name__ == "__main__":
-    app = BitcoinToolsApp()
-    app.run()
+        app = BitcoinToolsApp()
+        app.run()
