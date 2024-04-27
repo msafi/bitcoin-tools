5ba804b7941ba7b1d8c2aea076411be060effacd
DragonBlade4419 <166095567+DragonBlade4419@users.noreply.github.com>
2024-04-27 05:45:05 +0000

    Jhopison

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from textual import on
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Label, Input, Button, Static
 from textual.containers import Center
@@ -9,13 +10,17 @@ class BitcoinToolsApp(App):
             with Center():
                 yield Label("Welcome, human!\nwhat name be?", id="one")
             with Center():
-                yield Input(placeholder="First Name")
+                yield Input(placeholder="First Name Ex=Jhopsion")
                 yield Input(placeholder="Last Name")
+                yield Button("Primary!", variant="primary")
             yield Footer()
         def action_toggle_dark(self) -> None:
                         self.dark = not self.dark
         def on_mount(self) -> None:
                         self.title = "Open Wallet"
+        @on(Button.Pressed)
+        def are_there_set_thing_on_what_to_call_this_thing(self):
+          yield Button("Primary!", variant="primary"),
 if __name__ == "__main__":
         app = BitcoinToolsApp()
         app.run()
